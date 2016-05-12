3d29fcb7de1a680271044eef9272d288d19fc9be
ngoctho12 <dangngoctho.xd@gmail.com>
2016-05-12 20:31:49 +0700

    Update aodongphuclop3.js

--- aodongphuclop3.js
+++ aodongphuclop3.js
@@ -5,5 +5,4 @@
         google.annotations.fetch();
       }
       google.setOnLoadCallback(initialize);
-      function bp_thumbnail_resize(e,d){var c=1600;var a=true;var b="http://2.bp.blogspot.com/-erTXCq61ULM/TmHYAQBZ0GI/AAAAAAAACCs/6cBX54Dn6Gs/s1600/default.png";if(a==true&&e==""){e=b}image_tag='<img src="'+e.replace("/s72-c/","/s"+c+"/")+'" class="postthumb" alt="'+d+'" width="180px" height="130px"/>';if(e!=""){return image_tag}else{return""}};
-      document.write(bp_thumbnail_resize(&quot;<data:post.thumbnailUrl/>&quot;,&quot;<data:post.title/>&quot;));
+    document.write(bp_thumbnail_resize(&quot;<data:post.thumbnailUrl/>&quot;,&quot;<data:post.title/>&quot;));
